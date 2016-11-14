246b3a6a92655e9a9651625594ab989d864f2194
Sungchul Choi <sc82.choi@gachon.ac.kr>
2016-11-14 15:46:16 +0900

    update codes

--- code/11/finally_exception.py
+++ code/11/finally_exception.py
@@ -1,8 +1,8 @@
-try:
-    for i in range(1, 10):
+for i in range(0, 10):
+    try:
         result = 10 // i
-        print(result)
-except ZeroDivisionError:
-    print("Not divided by 0")
-finally:
-    print("종료되었습니다.")
+        print(i, "------", result)
+    except ZeroDivisionError:
+        print("Not divided by 0")
+    finally:
+        print("종료되었습니다.")
--- code/12/file_write.py
+++ code/12/file_write.py
@@ -4,8 +4,7 @@ for i in range(1, 11):
     f.write(data)
 f.close()
 
-
 with open("count_log.txt", 'a', encoding="utf8") as f:
-    for i in range(1, 11):
+    for i in range(100, 111):
         data = "%d번째 줄입니다.\n" % i
         f.write(data)
